40a193d6913490c204b22790bc3147df9e01d990
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-16 18:19:23 +0500

    fixed

--- card.py
+++ card.py
@@ -17,7 +17,7 @@ class Card(Randommer):
 
         
         headers = {
-            "X-Api-Key": "f1ab06cd2da14928a4f4299e85162d76"
+            "X-Api-Key": api_key
         }
 
         response = requests.get(url, headers=headers)
